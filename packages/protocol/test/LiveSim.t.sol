d92622ad42898d51499181e622d2d9f96a846f99
DigiClums <vijaykewlani231994@gmail.com>
2026-07-26 04:33:12 +0000

    test(protocol): make live fork test CI-safe

--- packages/protocol/test/LiveSim.t.sol
+++ packages/protocol/test/LiveSim.t.sol
@@ -6,16 +6,58 @@ import '../src/ProtocolDirectory.sol';
 import '../src/controller/UnifyVaultController.sol';
 import '../src/vault/CustodyVault.sol';
 import '../src/token/UVBTCETHToken.sol';
-import '../src/strategy/PortfolioManager.sol';
-import '../src/strategy/StrategyManager.sol';
 import '../src/constants/ModuleIds.sol';
 import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 
+interface VmExt {
+  function createSelectFork(string calldata urlOrAlias) external returns (uint256);
+  function envOr(
+    string calldata key,
+    string calldata defaultValue
+  ) external returns (string memory);
+  function rpcUrl(string calldata rpcOrAlias) external returns (string memory);
+  function skip(bool skip) external;
+}
+
 contract LiveSimTest is Test {
+  VmExt internal constant vmExt = VmExt(address(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D));
+
   address public constant PROTOCOL_DIRECTORY = 0xDd29e54f91b86f3e4609AA2e279e04E98dcAb722;
   address public constant BASE_SEPOLIA_USDC = 0x036CbD53842c5426634e7929541eC2318f3dCF7e;
 
+  function _shouldSkip() internal returns (bool, string memory) {
+    string memory rpcUrl;
+    try vmExt.envOr('BASE_SEPOLIA_RPC_URL', '') returns (string memory res) {
+      rpcUrl = res;
+    } catch {}
+    if (
+      bytes(rpcUrl).length == 0 ||
+      keccak256(bytes(rpcUrl)) == keccak256(bytes('${BASE_SEPOLIA_RPC_URL}'))
+    ) {
+      return (true, '');
+    }
+    return (false, rpcUrl);
+  }
+
+  function setUp() public {
+    (bool skipTest, string memory rpcUrl) = _shouldSkip();
+    if (skipTest) {
+      vmExt.skip(true);
+      return;
+    }
+    try vmExt.createSelectFork(rpcUrl) {} catch {
+      vmExt.skip(true);
+      return;
+    }
+  }
+
   function testLiveDepositAndRedeemOnFork() public {
+    (bool skipTest, ) = _shouldSkip();
+    if (skipTest) {
+      vmExt.skip(true);
+      return;
+    }
+
     address user = address(0x1234567890123456789012345678901234567890);
     ProtocolDirectory directory = ProtocolDirectory(PROTOCOL_DIRECTORY);
 
